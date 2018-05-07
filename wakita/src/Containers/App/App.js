7fd3c5deb033d2dcc465edbc16b948f6ff3fcc29
Daniel Ward <dward1502@gmail.com>
2018-05-06 23:10:24 -0700

    working on integrating css modules and sass

--- wakita/src/Containers/App/App.js
+++ wakita/src/Containers/App/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.scss';
+console.log(styles);
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
@@ -19,3 +17,4 @@ class App extends Component {
 }
 
 export default App;
+
