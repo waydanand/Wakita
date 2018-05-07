100479449a663749e8027b71e236eb784bcbaf3b
MacbookPro <andreesito@gmail.com>
2018-05-06 23:55:17 -0700

    server.js created - packages installed - routes dir created

---
+++ server.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const apiRoutes = require('./routes').apiRoutes;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(apiRoutes);
+
+
+app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+
+app.listen(PORT);
+console.log('The magic happens on port ' + PORT);
