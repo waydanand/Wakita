5a6f97bd4d59805146dd6e31d761740fc5f9968d
MacbookPro <andreesito@gmail.com>
2018-05-09 14:23:44 -0700

    normalize added to head in index.html - header component created

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = props => {
+    return (
+        <header>
+            <h1>Welcome</h1>
+            <p>Thank you for visiting our site. We hope that we can assist you with all your real estate queries and needs.</p>
+        </header>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
