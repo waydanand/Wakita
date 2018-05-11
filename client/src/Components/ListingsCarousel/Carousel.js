7d0037585a9b8e6f9c419103dc2a5f8902a4796a
MacbookPro <andreesito@gmail.com>
2018-05-11 15:16:24 -0700

    react siema carousel component created - basic functionality - not styled and text overlay still needs to be created

---
+++ client/src/Components/ListingsCarousel/Carousel.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import ReactSiema from 'react-siema'
+import styles from './Carousel.scss';
+
+class Carousel extends Component {
+
+constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+}
+
+Slide = (props) => <img {...props} alt='slide' />;
+
+render() {
+    let slider;
+    const Slide = (props) => <img {...props} alt='slide' />;
+    const options = {
+            resizeDebounce: 250,
+            duration: 1000,
+            easing: 'ease-in',
+            perPage: 1,
+            startIndex: 0,
+            draggable: true,
+            threshold: 20,
+            loop: true
+    };
+        
+
+        return (
+            <div className={styles.siemaCarousel}>
+                <ReactSiema {...options} ref={siema => slider = siema}>
+                    <Slide src='http://sandiegodreamrealestate.com/wp-content/uploads/sites/208/2018/04/6326-lake-athabaska-pl-007-1024x683.jpg' />
+                    <Slide src='http://sandiegodreamrealestate.com/wp-content/uploads/sites/208/2018/04/6326-lake-athabaska-pl-007-1024x683.jpg' />
+                    <Slide src='http://sandiegodreamrealestate.com/wp-content/uploads/sites/208/2018/04/6326-lake-athabaska-pl-007-1024x683.jpg' />
+                    <Slide src='http://sandiegodreamrealestate.com/wp-content/uploads/sites/208/2018/04/6326-lake-athabaska-pl-007-1024x683.jpg' />
+                </ReactSiema>
+
+                <button className={styles.prev} onClick={() => slider.prev()}>prev</button>
+                <button className={styles.next} onClick={() => slider.next()}>next</button>
+            </div>
+
+        );
+    }
+}
+
+export default Carousel;
--- client/src/Components/Pages/Home/Home.js
+++ client/src/Components/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Contact from '../../UI/ContactForm/ContactForm';
+import ListingsCarousel from '../../ListingsCarousel/Carousel';
 
 const Home = () => {
    return (
       <div>
-        
+        <ListingsCarousel />
         <Contact title='Have any Questions?'/>
       </div>
    );
