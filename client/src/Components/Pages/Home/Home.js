1a71671d2d98214cb09bd85e1f6c61bbc647edbf
MacbookPro <andreesito@gmail.com>
2018-05-14 11:10:47 -0700

    working on css grid

--- client/src/Components/Pages/Home/Home.js
+++ client/src/Components/Pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Contact from '../../UI/ContactForm/ContactForm';
 import ListingsCarousel from '../../ListingsCarousel/Carousel';
+import styles from './Home.scss';
 
 const Home = () => {
    return (
       <div>
-        <ListingsCarousel />
+        <ListingsCarousel className={styles.listOnGrid}/>
         <Contact title='Have any Questions?'/>
       </div>
    );
